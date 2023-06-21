9d82ba40a8b49868baae13c39fc2df4a88a236e2
JordanEngland <lcplengland@gmail.com>
2023-06-21 18:42:30 -0500

    updated more files, trying to test in insomina

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,17 +1,26 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Define model associations
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-
+// Export models and associations
 module.exports = {
   Product,
   Category,
